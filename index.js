20f4d3d98a0542c2ed1ab84db78f3dd51be30a1f
Jason Lough <jlough@backroomwebdesign.com>
2015-07-05 21:59:24 -0500

    much of the main functionality is there now, biggest issue is handling users leaving.

--- index.js
+++ index.js
@@ -3,45 +3,62 @@ var app = express();
 var http = require('http').Server(app); //the server object
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-	
-	res.sendFile(__dirname + '/index.htm'); //sends a file
-});
-
 app.use(express.static(__dirname + '/public')); 
 
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.htm'); //sends a file when / is the path
+});
 
-var userIDlist = [];
-var connected = 0;
-var thisUserID = "";
+app.get('/ex1', function(req, res) {
+	res.sendFile(__dirname + '/ex1.htm'); //for experiments
+});
+
+var userIDdict = {}; //will hold {userName : userID}
+var connected = 0; //number of connected users
+var totalEverConnected = 0;
 
 io.on('connection', function(socket) {
-	console.warn('a user connected');
 	
+	totalEverConnected++;
 	connected++;
-	thisUserID = generateUserID();
-	userIDlist.push(thisUserID);
 
-	io.emit('update connected count', connected);
-	io.emit('user joined', thisUserID);
+	//mask the socket.id from public view, b/c idk wtf it may permit
+	userName = generateUserID();
+	userID = socket.id;
+
+	userIDdict[userName] = userID; 
+
+	//push the userName to the client that just connected
+	io.sockets.connected[userID].emit('identity', userName);
+
+	//tell everyone someone joined, and how many current connected is
+	io.emit('join', {conn:connected, un:userName});	
+
+	console.log('socket.id : ' + socket.id);
+
+	socket.on('drip', function(letter) {//someone posted letter
+		
+		io.emit('drop', {content:letter, usr:userName}); 
+		console.log('emitting a drop : %s', letter);
+		console.log('socket.id : ' + socket.id);
+	});
 
 	socket.on('disconnect', function() {
 		connected--;
 
-		userIDlist.splice(userIDlist.indexOf(thisUserID), 1); //removes this user from the userIDlist[]
+		if(userIDdict['UserID']) {
+			delete(userIDdict['UserID']); //removes this user from the userIDlist[]
+
+		}
+
+		io.emit('user left', userID);
 
 		io.emit('update connected count', connected);
-		io.emit('user left', thisUserID);
 
 		console.warn('a user disco neck ted');
 	});
 
-	socket.on('chat message', function(msg) { //no msg! lol
-		console.log('chat_message : %s', msg );
-		io.emit('chat message', msg);
-
-	});
-
+	console.log('%s user connected', userName);
 });
 
 function generateUserID() {
@@ -51,7 +68,7 @@ function generateUserID() {
 		id += String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 	}
 
-	//console.log(id);
+	//console.log('generated id : %s', id);
 	return id;
 }
 
--- public/scripts.js
+++ public/scripts.js
@@ -1,29 +1,68 @@
 var socket = io();
 
-//whenever the value of #userinput changes, emit that, and clear it
-$('#userinput').keypress(function(code) {
-	console.log('%s', String.fromCharCode(code.charCode));
-	socket.emit('drip', $('#userinput').val());
-	$('#userinput').val('');
-});
+$( document ).ready( function() { 
+
+  //////////////////following are local vars
+  var myIdentity = '';
 
 
+  ////////////////following are socket events
 
-socket.on('drip', function(drop) {
-  console.log('drip detected');
-  $('#aggregate').append(drop);
-});
-
-socket.on('update connected count', function(msg) {
-  $('#connectedCount').html(msg);
-});
-
-socket.on('user joined', function(msg) {
-  $('#logger').html(msg).fadeOut(2500);
-});
-
-socket.on('user left', function(msg) {
-  $('#logger').html(msg).fadeOut(2500, function(){
+  socket.on('identity', function(msg){
+    myIdentity = msg;
+    $('#myid span').html(msg);
+    console.log('you');
 
   });
+
+
+  socket.on('drop', function(msg) {
+    //add to allChars{}, then call fn to display allChars in #aggregate
+    console.log(msg.usr);
+    $('#aggregate').append("<span class='" + msg.usr + "'>" + msg.content + "</span>");
+
+  });
+ 
+
+  socket.on('join', function(msg) {
+    console.log('msg.connected:%s msg.userName:%s', msg.conn, msg.un);
+    $('#connected span').html(msg.conn);
+    $('#userlist ul').append('<li>' + msg.un + '</li>');
+    $('#notification').html("<span class='joined'>" + msg.un + '</span>').fadeTo(2500, 0.0);
+  });
+
+  socket.on('user left', function(msg) {
+    $('#notification').html("<span class='left'>" + msg + '</span>').fadeTo(2500, 0.0);
+  });
+
+
+  //////////////////following are UI interactions///////
+
+  //whenever the value of #userinput changes, emit that, and clear it
+  $('#userinput').keyup(function(code) {
+    var val = $('#userinput').val();
+    //console.log('%s', String.fromCharCode(code.charCode));
+    socket.emit('drip', val); 
+    $('#userinput').val('');
+    $('#mytext').append(val);
+  });
+
+
+  //clicking on an item in user list gets the ID element to search for in aggregate
+  $('ul').on('click', 'li', function() {
+    $('#usertext').html(''); //blank the usertext area
+
+    var extractID = "." + $(this).text();
+    //console.log(extractID);
+
+    var text = $(extractID).text();
+    //console.log(text);
+
+    $('#usertext').html(text);
+   
+  });
+  
+	
+
 });
+
